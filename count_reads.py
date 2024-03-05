6fa02814401782f75d6c95d1723c18f032299099
brunapilotto <m.brunapilotto@usp.br>
2024-03-05 20:43:05 -0300

    feat: count reads from .fastq in a directory

---
+++ count_reads.py
@@ -0,0 +1,31 @@
+import os
+from termcolor import colored
+import argparse
+
+
+def menu():
+    parser = argparse.ArgumentParser(description="Count reads from fastq files in a directory")
+    parser.add_argument("--path",
+                        required=True,
+                        help="Path to the directory with fastq files")
+    args = parser.parse_args()
+    return args
+
+
+def count_reads(file: str) -> int:
+    with open(file, 'r') as f:
+        lines = f.readlines()
+        return len(lines) / 4
+
+
+def count_reads_in_directory(directory: str) -> None:
+    files = [file for file in os.listdir(directory) if file.endswith('.fastq')]
+    files.sort()
+    for file in files:
+        print(f"Arquivo {colored(file, 'green')} possui {colored((count_reads(os.path.join(directory, file))), 'green')} reads")
+
+
+if __name__ == "__main__":
+    options = menu()
+    print(colored('Read count', 'magenta'))
+    count_reads_in_directory(options.path)
